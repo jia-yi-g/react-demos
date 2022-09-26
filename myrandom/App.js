6868a6fef8878557f2d141cf69a8f163425983b4
malamenteee <83875657+jia-yi-g@users.noreply.github.com>
2022-09-26 22:26:26 +0800

    16-myrandomperson

---
+++ myrandom/App.js
@@ -0,0 +1,124 @@
+import React, { useState, useEffect } from 'react'
+import {
+  FaEnvelopeOpen,
+  FaUser,
+  FaCalendarTimes,
+  FaMap,
+  FaPhone,
+  FaLock,
+} from 'react-icons/fa'
+const url = 'https://randomuser.me/api/'
+const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg'
+function App() {
+  const random=async()=>{
+    const response=await fetch(url)
+    const data=await response.json()
+    const person=data.results[0]
+    const{phone,email}=person
+    const {first,last}=person.name
+    const{number,name}=person.location.street
+    const {password}=person.login
+    const{large:image}=person.picture
+    const {age}=person.dob
+    const newPerson={
+      phone,email,password,age,image,number,age,
+      name: `${first} ${last}`,
+      street:`${number} ${name}`
+    }
+    setPerson(newPerson)
+    setShow('name') 
+    setValue(newPerson.name)
+  }
+  useEffect(random,[])
+  
+  const[person,setPerson]=useState({})
+  const [show,setShow]=useState('name')
+  const [value,setValue]=useState('')
+  
+  return (
+    <main>
+      <div className="section">
+        <div className="section-center">
+          <div className="bcg-black block"></div>
+            <div className="block">
+              <div className="container">
+                <img src={person.image}>
+                </img>
+                {/* 当person是null时显示默认图像 */}
+                <p className="user-title">{`My ${show} is`}</p>
+                <p className="user-value">{value}</p>
+                <div className="values-lists">
+                  <button
+                    className="icon"
+                    
+                    onMouseOver={() => {
+                      setShow("email");
+                      setValue(person.email)
+                    }}
+                  >
+                    <FaEnvelopeOpen />
+                  </button>
+                  <button
+                    className="icon"
+                    onMouseOver={() => {
+                      setShow("name");
+                      setValue(person.name)
+                    }}
+                  >
+                    <FaUser />
+                  </button>
+                  <button
+                    className="icon"
+                    onMouseOver={() => {
+                      setShow("age");
+                      setValue(person.age)
+                    }}
+                  >
+                    <FaCalendarTimes />
+                  </button>
+                  <button
+                    className="icon"
+                    onMouseOver={() => {
+                      setShow("street");
+                      setValue(person.street)
+                    }}
+                  >
+                    <FaMap />
+                  </button>
+                  <button
+                    className="icon"
+                    onMouseOver={() => {
+                      setShow("phone");
+                      setValue(person.phone)
+                    }}
+                  >
+                    <FaPhone />
+                  </button>
+                  <button
+                    className="icon"
+                    onMouseOver={() => {
+                      setShow("password");
+                      setValue(person.password)
+                    }}
+                  >
+                    <FaLock />
+                  </button>
+                </div>
+                <button
+                  className="btn"
+                  onClick={() => {
+                    setPerson(random());
+                  }}
+                >
+                  RANDOM USER
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      
+    </main>
+  );
+}
+
+export default App
